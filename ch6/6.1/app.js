cc685a19be3ebcb1fc1f30e62c9acbccb6fdff0b
Kim Min Uk <62949434+qjvk2880@users.noreply.github.com>
2022-05-18 17:48:56 +0900

    ㅇ

--- ch6/6.1/app.js
+++ ch6/6.1/app.js
@@ -1,18 +1,29 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use((req, res, next) => {
+    console.log("모든 요청에 실행하고 싶어요");
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.send("hello epress");
+    res.sendFile(path.join(__dirname,"./index.html"));
 });
 app.post('/', (req, res) => {
     res.send("hello express");
 });
+
+
 app.get('/about', (req, res) => {
     res.send("hello express");
 });
 
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.send("에러")
+});
 app.listen(app.get('port'), () => {
     console.log("서버 실행");
 })
